963c276816aaa04eeec9916679fdc806bb3f11de
Declan <laresearchlabs@gmail.com>
2024-07-25 18:46:22 +0100

    add LessonType and Instructor models

--- bookalesson/admin.py
+++ bookalesson/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import LessonType, Instructor
 
 # Register your models here.
+admin.site.register(LessonType)
+admin.site.register(Instructor)
--- bookalesson/models.py
+++ bookalesson/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Instructor(models.Model):
+    name = models.CharField(max_length=200)
+    bio = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
+
+class LessonType(models.Model):
+    LEVEL_CHOICES = (
+        ('beginner', 'Beginner'),
+        ('intermediate', 'Intermediate'),
+        ('advanced', 'Advanced'),
+    )
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    level = models.CharField(max_length=20, choices=LEVEL_CHOICES)
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE, related_name='lesson_types')
+    content = models.TextField()
+
+
