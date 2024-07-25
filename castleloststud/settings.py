1f662801a93a3e5ac737c504e5fcdd72631747ba
Declan <laresearchlabs@gmail.com>
2024-07-25 12:35:29 +0100

    ready to deploy to heroku

--- castleloststud/settings.py
+++ castleloststud/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-dv7--kneg@d#&@#ri)8(+n6nhs(v24(xq=4yfw+6u-s9(m9@ox"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
