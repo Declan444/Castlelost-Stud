dc9c0e14ecf53ecf36bf96f2c0bbf83336f508ac
Declan <laresearchlabs@gmail.com>
2024-07-26 22:18:46 +0100

    add lessons_details view

--- bookalesson/urls.py
+++ bookalesson/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.LessonTypesList.as_view(), name='home'),
+    path('<slug:slug>/', views.lessons_detail, name='lessons_detail'),
 ]
--- bookalesson/views.py
+++ bookalesson/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Lessons
 
@@ -7,4 +7,17 @@ from .models import Lessons
 class LessonTypesList(generic.ListView):
     queryset = Lessons.objects.all()
     template_name = 'bookalesson/index.html'
+
+
+def lessons_detail(request, slug):
+    
+
+    queryset = Lessons.objects.all()
+    lesson = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "bookalesson/lessons_detail.html",
+        {"lessons": lesson},
+    )
     
