1e9d12faf64e8e608ea4d9076d03211c134a7f68
Declan <laresearchlabs@gmail.com>
2024-08-01 12:50:21 +0100

    modify lessons_details view to only allow user to comment on a lesson date that they have had

--- bookalesson/forms.py
+++ bookalesson/forms.py
@@ -1,2 +1,21 @@
+from .models import CommentOnLesson, LessonDate
+from django import forms
 
+
+class CommentForm(forms.ModelForm):
+    lesson_date = forms.ModelChoiceField(
+        queryset=LessonDate.objects.none(),  # Placeholder; will be set dynamically in the view
+        required=True,
+        empty_label="Select a lesson date",
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    class Meta:
+        model = CommentOnLesson
+        fields = ['text', 'lesson_date']  # Include lesson_date in the form fields
+
+    def __init__(self, *args, **kwargs):
+        lesson_dates = kwargs.pop('lesson_dates', LessonDate.objects.none())
+        super().__init__(*args, **kwargs)
+        self.fields['lesson_date'].queryset = lesson_dates
 
--- bookalesson/views.py
+++ bookalesson/views.py
@@ -3,7 +3,8 @@ from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 
-from .models import Lesson 
+from .models import Lesson, CommentOnLesson, LessonDate 
+from .forms import CommentForm
 
 
 
@@ -23,11 +24,41 @@ class LessonList(generic.ListView):
 #    return render(request, 'bookalesson/index.html', {'lesson_list': lessons})
 
 def lessons_detail(request, slug):
+    lesson = get_object_or_404(Lesson, slug=slug)
+    lesson_dates = LessonDate.objects.filter(lesson=lesson).order_by('date', 'start_time')
     
-    queryset = Lesson.objects.all()
-    lesson = get_object_or_404(queryset, slug=slug)
-    comments = lesson.comments.all().order_by("-created_on")
-    comment_count = lesson.comments.filter(approved=True).count()
+    # Check if the user has already commented on this lesson
+    existing_comment = None
+    if request.user.is_authenticated:
+        existing_comment = CommentOnLesson.objects.filter(
+            author=request.user,
+            lesson_type=lesson
+        ).first()
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST, lesson_dates=lesson_dates)
+        if comment_form.is_valid():
+            # Check if the user has already submitted a comment for this lesson date
+            lesson_date = comment_form.cleaned_data['lesson_date']
+            if CommentOnLesson.objects.filter(
+                author=request.user,
+                lesson_type=lesson,
+                lesson_date=lesson_date
+            ).exists():
+                comment_form.add_error(None, "You have already submitted a comment for this lesson on this date.")
+            else:
+                # Save the new comment
+                comment = comment_form.save(commit=False)
+                comment.author = request.user
+                comment.lesson_type = lesson
+                comment.save()
+                return redirect('lessons_detail', slug=slug)  # Redirect after POST to avoid resubmission
+    else:
+        comment_form = CommentForm(lesson_dates=lesson_dates)
+
+    comments = CommentOnLesson.objects.filter(lesson_type=lesson).order_by("-created_on")
+    comment_count = comments.filter(approved=True).count()
+
     return render(
         request,
         "bookalesson/lessons_detail.html",
@@ -35,6 +66,7 @@ def lessons_detail(request, slug):
             "lesson": lesson,
             "comments": comments,
             "comment_count": comment_count,
-         
-         },
+            "comment_form": comment_form,
+            "existing_comment": existing_comment,
+        },
     )
