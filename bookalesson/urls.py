9e86480664e91a90190b90efe70916cf471474da
Declan <laresearchlabs@gmail.com>
2024-08-19 16:45:34 +0000

    fix edit comment error by removing trailing  slash

--- bookalesson/urls.py
+++ bookalesson/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('lessons/', views.LessonList.as_view(), name='lessons'),
     
-    path('<slug:slug>/edit_comment/<int:comment_id>/', views.comment_edit, name='comment_edit'),
-    path('<slug:slug>/delete_comment/<int:comment_id>/', views.comment_delete, name='comment_delete'),
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
     path('book-a-lesson/', views.book_a_lesson, name='book_a_lesson'),
     path('timeslots/<date>/', views.timeslots_for_date, name='timeslots_for_date'),
     path('booking/<str:date>/<str:slot>/', views.booking_form, name='booking_form'),
--- static/js/comments.js
+++ static/js/comments.js
@@ -13,15 +13,14 @@ for (let button of editButtons) {
     let commentContent = document.getElementById(`comment${commentId}`).innerText;
     commentText.value = commentContent;
     submitButton.innerText = "Update";
-    commentForm.setAttribute("action", `edit_comment/${commentId}`);
+    commentForm.setAttribute("action", `/${slug}/edit_comment/${commentId}/`); // Add trailing slash
   });
 }
 
-
 for (let button of deleteButtons) {
-    button.addEventListener("click", (e) => {
-      let commentId = e.target.getAttribute("comment_id");
-      deleteConfirm.href = `delete_comment/${commentId}`;
-      deleteModal.show();
-    });
-  }
+  button.addEventListener("click", (e) => {
+    let commentId = e.target.getAttribute("comment_id");
+    deleteConfirm.href = `/${slug}/delete_comment/${commentId}/`; // Add trailing slash
+    deleteModal.show();
+  });
+}
