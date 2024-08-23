4f80f0495fdf0d239cd022006cceae8d5f0cebb0
Declan <laresearchlabs@gmail.com>
2024-08-23 21:42:39 +0000

    update static files with collectstatic

--- about/views.py
+++ about/views.py
@@ -4,7 +4,6 @@ from .models import About
 # Create your views here.
 def about_me(request):
     about_list = About.objects.all().order_by('-updated_on')
-    print(f"about_list contains: {about_list}")  # Debugging line
 
     return render(
         request,
