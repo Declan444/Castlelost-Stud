28ec65de0960688e021f1f5f25bf28dc2d03f3fb
Declan <laresearchlabs@gmail.com>
2024-09-09 22:44:45 +0000

    update settings to accept a responsive site for screenshot for readme

--- castleloststud/settings.py
+++ castleloststud/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'castlelost-stud-cbac18c58c5b.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'castlelost-stud-cbac18c58c5b.herokuapp.com', 'ui.dev/amiresponsive']
 
 
 # Application definition
@@ -159,4 +159,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CSP_FRAME_ANCESTORS = ("'self'", "http://ami.responsivedesign.is")
+CSP_FRAME_ANCESTORS = ("'self'", "https://ui.dev/amiresponsive")
